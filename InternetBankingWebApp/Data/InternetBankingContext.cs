86907e3c0cfd5e810b3d07215d9a858143996884
Chenyu Xiao <s3829221@student.rmit.edu.au>
2021-01-20 16:46:40 +1100

    Create Customer model

---
+++ InternetBankingWebApp/Data/InternetBankingContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetBankingWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternetBankingWebApp.Data
+{
+    public class InternetBankingContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+        public InternetBankingContext(DbContextOptions<InternetBankingContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Validations using Fluent API
+            builder.Entity<Customer>();
+        }
+
+    }
+}
---
+++ InternetBankingWebApp/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternetBankingWebApp.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustomerID { get; set; }
+
+        [Required, StringLength(50)]
+        public string CustomerName { get; set; }
+
+        [StringLength(11, MinimumLength = 11)]
+        public string TFN { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(20)]
+        public string State { get; set; }
+
+        [StringLength(4, MinimumLength = 4)]
+        public string PostCode { get; set; }
+
+        [Required, StringLength(15), RegularExpression(@"^\+61 [0-9]{4} [0-9]{4}$")]
+        public string Phone { get; set; }
+    }
+}
