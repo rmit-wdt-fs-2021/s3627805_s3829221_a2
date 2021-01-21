efad50e0a4d5c30e0b6370a0b2845ab08f565355
Chenyu Xiao <s3829221@student.rmit.edu.au>
2021-01-22 05:34:20 +1100

    Implement back-end login and deposit

--- InternetBankingWebApp/Controllers/HomeController.cs
+++ InternetBankingWebApp/Controllers/HomeController.cs
@@ -13,20 +13,18 @@ namespace InternetBankingWebApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
