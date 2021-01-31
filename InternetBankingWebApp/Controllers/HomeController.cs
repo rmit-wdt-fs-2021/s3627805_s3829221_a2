bf50805c9893c4ba812569f679938ba7a93a1b69
s3627805 <s3627805@student.rmit.edu.au>
2021-01-31 18:17:16 +0800

    Add Privacy

--- InternetBankingWebApp/Controllers/HomeController.cs
+++ InternetBankingWebApp/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace InternetBankingWebApp.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
